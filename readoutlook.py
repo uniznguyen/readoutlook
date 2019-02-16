43d05c33e5c016a866250f8133bcffd308d2c32c
uniznguyen <nguyen_uniz@yahoo.ca>
2019-02-16 09:09:27 -0600

    rework whole thing

--- readoutlook.py
+++ readoutlook.py
@@ -1,6 +1,6 @@
 import win32com.client
 import re
-
+from datetime import datetime
 
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
@@ -12,21 +12,32 @@ inbox = outlook.GetDeFaultFolder(6)
 Routepad = inbox.Folders("Route Pad sales")
 
 # filter emails where received time from 01/01/2018
-messages = Routepad.Items.Restrict("[ReceivedTime] >= '01/01/2019'")
+messages = Routepad.Items.Restrict("[ReceivedTime] >= '01/01/2018'")
 
 # sort by received time
 messages.Sort("[ReceivedTime]")
 
+msg = []
+
 # loop through messages
 if messages:
-    for message in messages:
-        
-        #get the body of each emails
-        body_content = message.body
+	for message in messages:
+		if "MCNi Remote Sales Processing for JT" in message.subject:
+			body_content = message.body
+			invoice_dates = re.findall(r'\d{2}\/\d{2}\/\d{2}',body_content)
+			sent_date = re.findall(r'\d{2}\/\d{2}\/\d{4}',message.subject)
+			for i in sent_date:	
+				for y in invoice_dates:
+					invoice_date = datetime.strptime(y,'%m/%d/%y').date()
+					sent_date = datetime.strptime(i,'%m/%d/%Y').date()
+					send_month = sent_date.strftime('%B')
+					invoice_month = invoice_date.strftime('%B')
+					day_diff = (invoice_date - sent_date)
 
-        # use regular expression to find invoice date of 2017
-        result = re.search(r'\d{2}\/\d{2}\/(18)',body_content)
-
-        # if found the sync in 2018 but invoice dated 2017, print the subject line. 
-        if result:
-            print (message.subject, result.group())
+					if invoice_month != send_month:
+						print (message.subject,send_month, invoice_month, invoice_date, day_diff)
+					
+				
+    		
+    			
+    		
